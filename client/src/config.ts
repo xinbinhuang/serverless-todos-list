a0c5d485d69672d3191ce7fc3a50cb475d721347
Xinbin Huang <bin.huangxb@gmail.com>
2020-06-01 06:57:16 -0700

    Update frontend config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,10 +1,8 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'v3pzqter37'
-export const apiEndpoint = `https://v3pzqter37.execute-api.us-west-2.amazonaws.com/dev`
+const apiId = 'xgwpz39dla'
+export const apiEndpoint = `https://${apiId}.execute-api.us-west-2.amazonaws.com/prod`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: 'koala-tree.auth0.com',            // Auth0 domain
+  domain: 'koala-tree.auth0.com',                        // Auth0 domain
   clientId: 'USgkMFXki7EtgnVkZ3shj5dVMcNYaI5u',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
